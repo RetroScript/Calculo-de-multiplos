4866dd0a11d330941c451aed2d4cf02fca1b0ebd
RetroScript <andrefrancelino06@gmail.com>
2023-08-10 17:54:48 -0300

    Publicação do Projeto
    Código fonte da aplicação publicado após a criação do repositório por ter sido previamente criado.

---
+++ MULTIPLOS DE UM NUMERO - CSHARP project/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Hello, world!");
+Console.WriteLine("Insira um numero para ver seus multiplos");
+
+string? userInput;
+int numeroEscolhido;
+int multiploResultante;
+
+Console.WriteLine("O numero escolhido é: ");
+userInput = Console.ReadLine();
+numeroEscolhido = Convert.ToInt32(userInput);
+
+Console.WriteLine($"TABUADA DO: {numeroEscolhido}");
+
+for (int i = 0; i <= 10; i++)
+{
+    multiploResultante = numeroEscolhido * i;
+
+    Console.WriteLine($"{numeroEscolhido} X {i} = {multiploResultante}");
+}
